11cc1a017a9edb122946527e1d2a1c01444006cb
Nima Khajehpour <nima1038khajehpour@gmail.com>
2023-01-28 22:09:18 +0330

    Updated The App so description is not required and deleting needs confirmation

--- app/src/main/java/com/nima/taskmanager/data/TaskDatabase.kt
+++ app/src/main/java/com/nima/taskmanager/data/TaskDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 
 @TypeConverters(UUIDConverter::class)
-@Database(entities = [Task::class, Note::class], version = 1, exportSchema = false)
+@Database(entities = [Task::class, Note::class], version = 2, exportSchema = false)
 abstract class TaskDatabase: RoomDatabase() {
     abstract fun taskDao(): TaskDao
 }
